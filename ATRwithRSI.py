82094019df65768070f4ccdb85a4a8495ec9b274
AIDevKSH <aidevksh@gmail.com>
2024-04-08 20:23:17 +0900

    'try-except'

--- ATRwithRSI.py
+++ ATRwithRSI.py
@@ -27,144 +27,178 @@ symbol = 'DOGEUSDT'
 interval = '30m'
 leverage = 5
 
+import time
+
 def post_leverage():
-    resp = binance.fapiprivate_post_leverage({
-        'symbol': symbol,
-        'leverage': leverage,
-    })
-    time.sleep(0.5)
-    return resp
+    try:
+        resp = binance.fapiprivate_post_leverage({
+            'symbol': symbol,
+            'leverage': leverage,
+        })
+        time.sleep(0.5)
+        return resp
+    
+    except Exception as e:
+        print("post_leverage() Exception:", e)
 
 def get_ohlc():
-    end_time = datetime.now()
-    start_time = end_time - timedelta(days=14)
+    try:
+        end_time = datetime.now()
+        start_time = end_time - timedelta(days=14)
 
-    start_timestamp = int(start_time.timestamp() * 1000)
-    end_timestamp = int(end_time.timestamp() * 1000)
+        start_timestamp = int(start_time.timestamp() * 1000)
+        end_timestamp = int(end_time.timestamp() * 1000)
 
-    klines = client.get_historical_klines(symbol, interval, start_timestamp, end_timestamp)
+        klines = client.get_historical_klines(symbol, interval, start_timestamp, end_timestamp)
 
-    ohlc_data = []
-    for kline in klines:
-        timestamp = datetime.fromtimestamp(kline[0] / 1000).strftime('%Y-%m-%d %H:%M:%S')
-        open_price = float(kline[1])
-        high_price = float(kline[2])
-        low_price = float(kline[3])
-        close_price = float(kline[4])
-        volume = float(kline[5])
-        ohlc_data.append([timestamp, open_price, high_price, low_price, close_price, volume])
+        ohlc_data = []
+        for kline in klines:
+            timestamp = datetime.fromtimestamp(kline[0] / 1000).strftime('%Y-%m-%d %H:%M:%S')
+            open_price = float(kline[1])
+            high_price = float(kline[2])
+            low_price = float(kline[3])
+            close_price = float(kline[4])
+            volume = float(kline[5])
+            ohlc_data.append([timestamp, open_price, high_price, low_price, close_price, volume])
 
-    ohlc_df = pd.DataFrame(ohlc_data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+        ohlc_df = pd.DataFrame(ohlc_data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
 
-    ohlc_df['Timestamp'] = pd.to_datetime(ohlc_df['Timestamp'])
+        ohlc_df['Timestamp'] = pd.to_datetime(ohlc_df['Timestamp'])
 
-    time.sleep(0.5)
+        time.sleep(0.5)
 
-    return ohlc_df
+        return ohlc_df
+    except Exception as e:
+        print("get_ohlc() Exception:", e)
 
 def get_current_price(df):
-    df = df.tail(1)
-    current_price = float(df['Close'])
+    try :
+        df = df.tail(1)
+        current_price = float(df['Close'])
 
-    return current_price
+        return current_price
+    
+    except Exception as e:
+        print("get_current_price() Exception", e)
 
 def calculate_atr(df):
-    df['High-Low'] = df['High'] - df['Low']
-    df['High-PreviousClose'] = abs(df['High'] - df['Close'].shift(1))
-    df['Low-PreviousClose'] = abs(df['Low'] - df['Close'].shift(1))
-    df['TrueRange'] = df[['High-Low', 'High-PreviousClose', 'Low-PreviousClose']].max(axis=1)
+    try :
+        df['High-Low'] = df['High'] - df['Low']
+        df['High-PreviousClose'] = abs(df['High'] - df['Close'].shift(1))
+        df['Low-PreviousClose'] = abs(df['Low'] - df['Close'].shift(1))
+        df['TrueRange'] = df[['High-Low', 'High-PreviousClose', 'Low-PreviousClose']].max(axis=1)
 
-    period = 14
-    df['ATR'] = df['TrueRange'].rolling(period).mean()
+        period = 14
+        df['ATR'] = df['TrueRange'].rolling(period).mean()
 
-    df.drop(['High-Low', 'High-PreviousClose', 'Low-PreviousClose', 'TrueRange'], axis=1, inplace=True)
+        df.drop(['High-Low', 'High-PreviousClose', 'Low-PreviousClose', 'TrueRange'], axis=1, inplace=True)
+        
+        return df
     
-    return df
+    except Exception as e:
+        print("calculage_atr() Exception", e)
 
 def calculate_rsi(df):
-    df['Change'] = df['Close'].diff()
+    try :
+        df['Change'] = df['Close'].diff()
 
-    upward_change = df['Change'][df['Change'] > 0].mean()
-    downward_change = -df['Change'][df['Change'] < 0].mean()
+        upward_change = df['Change'][df['Change'] > 0].mean()
+        downward_change = -df['Change'][df['Change'] < 0].mean()
 
-    rsi = 100 - (100 / (1 + (upward_change / downward_change)))
+        rsi = 100 - (100 / (1 + (upward_change / downward_change)))
+        
+        return rsi
     
-    return rsi
+    except Exception as e:
+        print("calculate_rsi() Exception", e)
 
 def calculate_atr_trailing_stop(df):
-    atr_trailing_stop = pd.Series(index=df.index)
+    try:
+        atr_trailing_stop = pd.Series(index=df.index)
 
-    for i in range(1, len(df)):
-        n_loss = df.iloc[i]['ATR'] * 2
-        close = df.iloc[i]['Close']
-        prev_close = df.iloc[i - 1]['Close']
-        prev_atr_trailing_stop = atr_trailing_stop.iloc[i - 1]
+        for i in range(1, len(df)):
+            n_loss = df.iloc[i]['ATR'] * 2
+            close = df.iloc[i]['Close']
+            prev_close = df.iloc[i - 1]['Close']
+            prev_atr_trailing_stop = atr_trailing_stop.iloc[i - 1]
 
-        if close > prev_atr_trailing_stop and prev_close > prev_atr_trailing_stop:
-            atr_trailing_stop.iloc[i] = max(prev_atr_trailing_stop, close - n_loss)
-        elif close < prev_atr_trailing_stop and prev_close < prev_atr_trailing_stop:
-            atr_trailing_stop.iloc[i] = min(prev_atr_trailing_stop, close + n_loss)
-        elif close > prev_atr_trailing_stop:
-            atr_trailing_stop.iloc[i] = close - n_loss
-        else:
-            atr_trailing_stop.iloc[i] = close + n_loss
+            if close > prev_atr_trailing_stop and prev_close > prev_atr_trailing_stop:
+                atr_trailing_stop.iloc[i] = max(prev_atr_trailing_stop, close - n_loss)
+            elif close < prev_atr_trailing_stop and prev_close < prev_atr_trailing_stop:
+                atr_trailing_stop.iloc[i] = min(prev_atr_trailing_stop, close + n_loss)
+            elif close > prev_atr_trailing_stop:
+                atr_trailing_stop.iloc[i] = close - n_loss
+            else:
+                atr_trailing_stop.iloc[i] = close + n_loss
 
-    df['ATR_Trailing_Stop'] = atr_trailing_stop
+        df['ATR_Trailing_Stop'] = atr_trailing_stop
 
-    df = df.tail(20)
+        df = df.tail(20)
 
-    return df
+        return df
+    
+    except Exception as e:
+        print("calculate_atr_trailing_stop() Exception", e)
 
 def if_crossover(df):
-    #  0 : initial value, no crossover 
-    #  1 : upward crossover
-    # -1 : downward crossover
+    try :
+        #  0 : initial value, no crossover 
+        #  1 : upward crossover
+        # -1 : downward crossover
 
-    df = df.tail(2)
+        df = df.tail(2)
+        
+        prev_close = df.iloc[0]['Close']
+        close = df.iloc[1]['Close']
+        
+        prev_atr_trailing_stop = df.iloc[0]['ATR_Trailing_Stop']
+        atr_trailing_stop = df.iloc[1]['ATR_Trailing_Stop']
+
+        if close > atr_trailing_stop and prev_close < prev_atr_trailing_stop:
+            crossover = 1
+        elif atr_trailing_stop > close and prev_atr_trailing_stop < close:
+            crossover = -1
+        else:
+            crossover = 0
+
+        return crossover
     
-    prev_close = df.iloc[0]['Close']
-    close = df.iloc[1]['Close']
-    
-    prev_atr_trailing_stop = df.iloc[0]['ATR_Trailing_Stop']
-    atr_trailing_stop = df.iloc[1]['ATR_Trailing_Stop']
-
-    if close > atr_trailing_stop and prev_close < prev_atr_trailing_stop:
-        crossover = 1
-    elif atr_trailing_stop > close and prev_atr_trailing_stop < close:
-        crossover = -1
-    else:
-        crossover = 0
-
-    return crossover
+    except Exception as e:
+        print("if_crossover() Exception", e)
 
 def position_decision(df, crossover, rsi):
-    #  0 : Hold 
-    #  1 : Enter Long  Position, Close Short Position
-    # -1 : Enter Short Position, Close Long Position
-    #  2 : Close Long  Positon
-    # -2 : Close Short Position
-    df = df.tail(1)
-    close = df.iloc[0]['Close']
-    atr_trailing_stop = df.iloc[0]['ATR_Trailing_Stop']
+    try :
+        #  0 : Hold 
+        #  1 : Enter Long  Position, Close Short Position
+        # -1 : Enter Short Position, Close Long Position
+        #  2 : Close Long  Positon
+        # -2 : Close Short Position
+        df = df.tail(1)
+        close = df.iloc[0]['Close']
+        atr_trailing_stop = df.iloc[0]['ATR_Trailing_Stop']
 
-    if close > atr_trailing_stop and crossover == 1 and rsi > 55:
-        position = 1
-    elif close > atr_trailing_stop and crossover == 1 and rsi < 45:
-        position = 1
-    elif close < atr_trailing_stop and crossover == -1 and rsi > 55:
-        position = -1
-    elif close < atr_trailing_stop and crossover == -1 and rsi < 45:
-        position = -1
-    elif close < atr_trailing_stop and crossover == -1 and 45 <= rsi <= 55:
-        position = 2
-    elif close > atr_trailing_stop and crossover == 1 and 45 <= rsi <= 55:
-        position = -2
-    else:
-        position = 0
+        if close > atr_trailing_stop and crossover == 1 and rsi > 55:
+            position = 1
+        elif close > atr_trailing_stop and crossover == 1 and rsi < 45:
+            position = 1
+        elif close < atr_trailing_stop and crossover == -1 and rsi > 55:
+            position = -1
+        elif close < atr_trailing_stop and crossover == -1 and rsi < 45:
+            position = -1
+        elif close < atr_trailing_stop and crossover == -1 and 45 <= rsi <= 55:
+            position = 2
+        elif close > atr_trailing_stop and crossover == 1 and 45 <= rsi <= 55:
+            position = -2
+        else:
+            position = 0
 
-    return position
+        return position
+    
+    except Exception as e:
+        print("position_decision() Exception", e)
 
 def chart_analysis(): 
+    try :
         ohlc_df = get_ohlc()
         
         ohlc_df = calculate_atr(ohlc_df)
@@ -188,28 +222,40 @@ def chart_analysis():
         print("RSI :",rsi)
 
         return decision, ohlc_df
+    
+    except Exception as e:
+        print("chart_analysis() Exception", e)
 
 def get_balance():
-    balance = binance.fetch_balance(params={"type": "future"})
-    free_balance = balance['free']
-    usdt = free_balance['USDT']
-    time.sleep(0.5)
-    return usdt
+    try :
+        balance = binance.fetch_balance(params={"type": "future"})
+        free_balance = balance['free']
+        usdt = free_balance['USDT']
+        time.sleep(0.5)
+        return usdt
+    
+    except Exception as e:
+        print("get_balance() Exception", e)
 
 def calculate_amount(usdt, current_price):
-    if usdt > current_price:
-        amount = usdt / current_price / 3
-        amount = int(amount) - 1
-        return amount
-    elif usdt < current_price:
-        # 여기 나중에 다시 계산해야됨
-        amount = 0
-        return amount
-    else:
-        amount = 0
-        return amount
+    try :
+        if usdt > current_price:
+            amount = usdt / current_price / 3
+            amount = int(amount) - 1
+            return amount
+        elif usdt < current_price:
+            # 여기 나중에 다시 계산해야됨
+            amount = 0
+            return amount
+        else:
+            amount = 0
+            return amount
+        
+    except Exception as e:
+        print("calculate_amount() Exception", e)
 
 def before_trade(ohlc_df):
+    try:
         current_price = get_current_price(ohlc_df)
         time.sleep(0.5)
 
@@ -223,95 +269,114 @@ def before_trade(ohlc_df):
         time.sleep(0.5)
 
         return if_position, prev_amount, amount
+    
+    except Exception as e:
+        print("before_trade() Exception", e)
 
 def my_position():
-    #  if_position Value
-    #  0 : Initial Value, Have No Position
-    #  1 : Prev Positon is Long
-    # -1 : Prev Position is Short
+    try :
+        #  if_position Value
+        #  0 : Initial Value, Have No Position
+        #  1 : Prev Positon is Long
+        # -1 : Prev Position is Short
 
-    balance = binance.fetch_balance()
-    positions = balance['info']['positions']
+        balance = binance.fetch_balance()
+        positions = balance['info']['positions']
 
-    for position in positions:
-        if position["symbol"] == symbol:
-            if float(position['positionAmt']) != 0:
-                prev_amount = float(position['positionAmt'])
-                if prev_amount > 0 : 
-                    if_position = 1
-                else :
-                    if_position = -1
-                    prev_amount = abs(prev_amount)
-            else:
-                if_position = 0
-                prev_amount = 0
+        for position in positions:
+            if position["symbol"] == symbol:
+                if float(position['positionAmt']) != 0:
+                    prev_amount = float(position['positionAmt'])
+                    if prev_amount > 0 : 
+                        if_position = 1
+                    else :
+                        if_position = -1
+                        prev_amount = abs(prev_amount)
+                else:
+                    if_position = 0
+                    prev_amount = 0
 
-            return if_position, prev_amount
+                return if_position, prev_amount
+            
+    except Exception as e:
+        print("my_position() Exception", e)
 
 def buy(amount):
-    binance.create_market_buy_order(
-        symbol=symbol,
-        amount=amount,
-    )
-    time.sleep(0.5)
+    try :
+        binance.create_market_buy_order(
+            symbol=symbol,
+            amount=amount,
+        )
+        time.sleep(0.5)
+
+    except Exception as e:
+        print("buy() Exception", e)
 
 def sell(amount):
-    binance.create_market_sell_order(
-        symbol=symbol,
-        amount=amount,
-    )
-    time.sleep(0.5)
+    try :
+        binance.create_market_sell_order(
+            symbol=symbol,
+            amount=amount,
+        )
+        time.sleep(0.5)
+
+    except Exception as e:
+        print("sell() Exception", e)
 
 def if_trade(decision, ohlc_df):
-    if decision == 0:
-        return
-    
-    if_position, prev_amount, amount = before_trade(ohlc_df)
+    try :
+        if decision == 0:
+            return
+        
+        if_position, prev_amount, amount = before_trade(ohlc_df)
 
-    #  decision Value
-    #  0 : Hold 
-    #  1 : Enter Long  Position, Close Short Position (If I have)
-    # -1 : Enter Short Position, Close Long Position  (If I have)
-    #  2 : Close Long  Positon   (If I have)
-    # -2 : Close Short Position  (If I have)
+        #  decision Value
+        #  0 : Hold 
+        #  1 : Enter Long  Position, Close Short Position (If I have)
+        # -1 : Enter Short Position, Close Long Position  (If I have)
+        #  2 : Close Long  Positon   (If I have)
+        # -2 : Close Short Position  (If I have)
 
-    #  if_position Value
-    #  0 : Initial Value, Have No Position
-    #  1 : Prev Positon is Long
-    # -1 : Prev Position is Short
+        #  if_position Value
+        #  0 : Initial Value, Have No Position
+        #  1 : Prev Positon is Long
+        # -1 : Prev Position is Short
 
-    # decision == 1
-    if decision == 1 and if_position == -1:
-        buy(prev_amount)
-        buy(amount)
+        # decision == 1
+        if decision == 1 and if_position == -1:
+            buy(prev_amount)
+            buy(amount)
 
-    elif decision == 1 and if_position != -1 and if_position != 1:
-        buy(amount)
+        elif decision == 1 and if_position != -1 and if_position != 1:
+            buy(amount)
 
-    elif decision == 1 and if_position == 1:
-        return
+        elif decision == 1 and if_position == 1:
+            return
 
-    # decision == -1
-    elif decision == -1 and if_position == 1:
-        sell(prev_amount)
-        sell(amount)
+        # decision == -1
+        elif decision == -1 and if_position == 1:
+            sell(prev_amount)
+            sell(amount)
 
-    elif decision == -1 and if_position != 1 and if_position != -1:
-        sell(amount)
+        elif decision == -1 and if_position != 1 and if_position != -1:
+            sell(amount)
 
-    elif decision == -1 and if_position == -1 :
-        return
+        elif decision == -1 and if_position == -1 :
+            return
 
-    # decision == 2
-    elif decision == 2 and if_position == 1:
-        sell(amount)
+        # decision == 2
+        elif decision == 2 and if_position == 1:
+            sell(amount)
 
-    # decision == -2
-    elif decision == -2 and if_position == -1:
-        buy(amount)
-    
-    else:
-        return
+        # decision == -2
+        elif decision == -2 and if_position == -1:
+            buy(amount)
+        
+        else:
+            return
+        
+    except Exception as e:
+        print("if_trade() Exception", e)
 
 def every_30_min():
         time.sleep(10)
