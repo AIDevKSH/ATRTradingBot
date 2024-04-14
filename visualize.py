f8470e4eb365ccb0f4af03bd1348d54b277316b8
AIDevKSH <aidevksh@gmail.com>
2024-04-14 16:40:51 +0900

    'bugFixingDay'

--- visualize.py
+++ visualize.py
@@ -9,6 +9,7 @@ print("\n", print_df[['Timestamp', 'Open' ,'Close', 'EMA_14', 'ATR_Trailing_Stop
 ohlc.ohlc_df['Decision'] = 0  # 일단 모든 행에 대해 0으로 초기화
 
 # 2. Decision 값 설정 | 1 : Enter Long | -1 : Enter Short
+# 거래 진입 : Crossover == Decision 일 때
 ohlc.ohlc_df.loc[(ohlc.ohlc_df['Crossover'] == 1) & (ohlc.ohlc_df['Open'] >= ohlc.ohlc_df['EMA_14']), 'Decision'] = 1
 ohlc.ohlc_df.loc[(ohlc.ohlc_df['Crossover'] == -1) & (ohlc.ohlc_df['Open'] <= ohlc.ohlc_df['EMA_14']), 'Decision'] = -1
 
@@ -17,4 +18,4 @@ crossover_df =  ohlc.ohlc_df[ohlc.ohlc_df['Crossover'] != 0]
 print(crossover_df)
 
 # 4. chart.png와 같은 그래프 출력하기
-# ohlc.make_plot(ohlc.ohlc_df)
+ohlc.make_plot(ohlc.ohlc_df)
